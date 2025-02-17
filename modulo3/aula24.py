60ffcd5cb029ca3f942a8270682f52bfd59e7933
Thiago Campos <thgcamposantos@gmail.com>
2025-02-16 23:20:31 -0300

    aula 16/02/2025'

--- modulo3/aula24.py
+++ modulo3/aula24.py
@@ -1,4 +1,15 @@
-print('ola')
+nome = 'thiago'
+sobrenome = 'campos'
+idade = 24
+ano_de_nascimento = '03/03/2000'
+maior_idade = True
+altura = 1.70
 
 
+print(nome)
+print(sobrenome)
+print(idade)
+print(ano_de_nascimento)
+print(maior_idade)
+print(altura)
 
---
+++ modulo3/aula29.py
@@ -0,0 +1,7 @@
+nome = 'thiago'
+altura = 1.70
+peso = 75
+imc = (peso / (altura * altura))
+
+print(nome , 'tem  a altura ' , altura , ' de altura, pesa ' , peso , ' quilos e seu imc é ')
+print(imc)
